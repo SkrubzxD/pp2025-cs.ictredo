83d1db9dd6e68bf2512903e0f93ff236a17edb4c
SkrubzxD <153061648+SkrubzxD@users.noreply.github.com>
2024-12-05 14:41:48 +0700

    Listed functions in Practical

---
+++ 1.student.mark.py
@@ -0,0 +1,38 @@
+'''
+Make a new Python program
+• Name it «1.student.mark.py»
+• Use tuples, dicts, lists, NO objects/classes
+• Build a student mark management system
+Input functions:
+• Input number of students in a class
+• Input student information: id, name, DoB
+• Input number of courses
+• Input course information: id, name
+• Select a course, input marks for student in this course
+ Listing functions:
+• List courses
+• List students
+• Show student marks for a given course
+'''
+
+students = []
+studentID = []
+studentName = []
+courseID = []
+studentMark = []
+courseName = []
+
+#def inputStudentNumberInClass(int ID): 
+
+#def inputStudentInfo(int(ID), str name, str DoB):
+#def inputCourseNumber(int num):
+#def inputCourseInfo(id, name) :
+#def inputStudentMarkToCourse(int courseID):
+
+def listCourses():
+    for i in courseID:
+        print(courseName[i])
+def listStudents():
+    for i in studentID:
+        print(studentID[i])
+#def listStudentMarks(int courseID):
