b7fedd223534be779e485d73ea5025632f78a4f5
SkrubzxD <153061648+SkrubzxD@users.noreply.github.com>
2024-12-07 14:07:36 +0700

    Added Menu() and inputStudentInfo()

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -21,18 +21,63 @@ studentName = []
 courseID = []
 studentMark = []
 courseName = []
+studentDoB = []
 
 #def inputStudentNumberInClass(int ID): 
 
-#def inputStudentInfo(int(ID), str name, str DoB):
+def inputStudentInfo():
+    ID = input("ID: ")
+    name = input("Name: ")
+    DoB = input("DoB: ")
+    studentID.append(ID)
+    studentName.append(name)
+    studentDoB.append(DoB)
+    Menu()
+
+
 #def inputCourseNumber(int num):
 #def inputCourseInfo(id, name) :
 #def inputStudentMarkToCourse(int courseID):
 
 def listCourses():
+    print ("Courses: \n")
+
     for i in courseID:
         print(courseName[i])
+    Menu()
+
 def listStudents():
+    print("\n   Students: \n")
     for i in studentID:
-        print(studentID[i])
+        print(studentID.index(i)+1, ":", i)
+    Menu()
 #def listStudentMarks(int courseID):
+
+# Menu
+menu = '''
+===
+Select option
+1. Input Student Info
+2.
+3. list students
+4. list courses
+5. Exit
+===
+'''
+def Menu():
+    print(menu)
+    option = int(input("Enter number: "))
+    match option:
+        case 1: 
+            inputStudentInfo()
+        case 3:
+            listStudents()
+        case 4: 
+            listCourses()
+        case 5:
+            exit()
+        case default:
+            Menu()
+    
+#main
+Menu()
