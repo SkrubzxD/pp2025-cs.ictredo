c7732bbd00a2850e3471547c651bd363d7a5abaa
SkrubzxD <153061648+SkrubzxD@users.noreply.github.com>
2024-12-12 14:55:26 +0700

    Redefined Database Structure.

--- 2.student.mark.py
+++ 2.student.mark.py
@@ -2,10 +2,10 @@
 
 ''' 
 Input functions:
-• Input number of students in a class
-• Input student information: id, name, DoB
-• Input number of courses
-• Input course information: id, name
+• Input number of students in a class (Done)
+• Input student information: id, name, DoB 
+• Input number of courses (Done)
+• Input course information: id, name 
 • Select a course, input marks for student in this course
  Listing functions:
 • List courses
@@ -14,29 +14,71 @@ Input functions:
 Schema :
 • StudentID -> studentName, DoB
 • CourseID -> courseName, studentMark, studentID
-• getMark(StudentID, CourseID) -> studentMark
+• listStudentMark(CourseID) -> studentMark
 '''
-class Entity():
-    name = []
-    ID = []
-    def list():
-        pass
-    def input():
-        pass
+studentNameList = []
+studentIDList = []
+studentDoBList = []
+courseIDList = []
+courseNameList = []
+
+numberOfStudents = 0
+numberOfCourses = 0
+
+def inputnumberOfCourses():
+    numberOfCourses = input("Enter number of courses: ")
+    return numberOfCourses
+
+
+class Entity:
+    def __init__(self, name, ID):
+        self.name = name
+        self.ID = ID
+    
+    def toString():
+        print(f"Name: {self.name}\n ID: {self.ID}\n")
+
 class Student(Entity):
     super().name
     super().ID
-    DoB = []
+    DoB = ""
     # day, month, year = DoB.split("/")
-    
-    def list():
-        super().list()
+    def inputStudentInfo(): 
+        #init student info
+        name = super().input("Enter name: ")
+        ID = super().input("Enter ID: ")
+        DoB = super().input("Enter DoB: ")
+        super().nameList.append(name)
+        super().IDList.append(ID)
+        super().DoBList.append(DoB)
 
+    def toString():
+        super().toString()
+        print(f"DoB: {self.DoB}\n")
 
 class Course(Entity):
-    numberOfStudent = 0
+    numberOfStudents = 0
     studentMark = []
-    super().name
-    super().ID
-    #def listStudentMarks():
-    #def studentNumber
+    def __init__(self, name, ID):
+        super().__init__(name, ID)
+
+    def inputStudentsInClass(self, number):
+        numberOfStudents = input("Enter number of students: ")
+        return numberOfStudents
+
+
+def listStudentMarks(class_type = "Course", a = Course):
+    for i in a.studentMark:
+        print(f"Student: {a.studentMark.index(i)+1}\n Mark: {i}\n")
+
+#main 
+Student1 = Student()
+Student1.inputStudentInfo()
+Student1.toString()
+
+Course1 = Course()
+Course1.inputCourseInfo()
+
+# listStudents()
+# listCourses()
+listStudentMarks(Course = "Course1")
